5f13450d2987243176a6fd5182c8d972b06caafd
Bruno Serra <bbucalonserra@hotmail.com>
2025-01-26 14:57:14 -0300

    Adding Py Files
    Adding Py Files

---
+++ predict.py
@@ -0,0 +1,26 @@
+# %%
+import mlflow.client
+import pandas as pd
+
+import mlflow
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# %%
+
+client = mlflow.client.MlflowClient()
+version = max([int(i.version) for i in client.get_latest_versions("churn")])
+
+# %%
+model = mlflow.sklearn.load_model(f"models:/churn/{version}")
+
+# %%
+
+df = pd.read_csv("data/abt.csv", sep=",")
+df
+
+# %%
+X = df.head()[model.feature_names_in_]
+proba = model.predict_proba(X)
+proba
+
+# %%
